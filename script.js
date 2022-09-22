8d45858765103d1f7db5c7d4faa6446a00631d16
anowacki <adriannowacki007@gmail.com>
2022-09-22 18:10:24 +0200

    zmiana warstwy nadlesnictw

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ var mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/adryanque/cl3d4aqvh00
 
 var wms_service = "http://localhost:8080/geoserver/wms"
 var wfs_service = "http://localhost:8080/geoserver/ows"
-var wfs_service_url = "http://localhost:8080/geoserver/projekt_bory_tucholskie/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=projekt_bory_tucholskie%3Anadlesnictwa&outputFormat=application/json"
+var wfs_service_url = "http://localhost:8080/geoserver/projekt_bory_tucholskie/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=projekt_bory_tucholskie%3Anadlesnictwa_przyciete&outputFormat=application/json"
 
 async function getWFSgeojson(){
     try{
@@ -57,7 +57,9 @@ getWFSgeojson().then(data=> {
         onEachFeature: function (feature, layer){
             console.log(feature);
             var popupContent = "<div><b>" + feature.properties.reg_name + "</b></br>"
-            + feature.properties.ins_name + "</br>" + feature.properties.pow_wycinek_ha + "</div>";
+            + "Nadleśnictwo: " + feature.properties.ins_name + "</br>" + "Powierzchnia wycinek: " + feature.properties.pow_wycinek_ha + "</div>" + "</br>" 
+            + "Powierzchnia nadleśnictwa: " + feature.properties.area_km2 + "</br>" + "% pokrycia: " + feature.properties.p_cover + "</br>"
+            + "% straty: " + feature.properties.p_loss + "</br>" + "% przyrostu" + feature.properties.p_gain;
             layer.bindPopup(popupContent);
 
 
